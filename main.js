742b268bbaf4644560160f065ce43ab2a3c201a4
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2023-08-14 22:28:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import { Modal } from "./modal.js";
+import { AlertError } from "./alert-erro.js";
+import { calculateIMC, notNumber } from "./utils.js";
+
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+
+  const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height);
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open();
+    return;
+  }
+  AlertError.close();
+
+  let result = calculateIMC(weight, height);
+  displayResultMessage();
+};
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open();
+}
+inputWeight.oninput = () => AlertError.close();
+inputHeight.oninput = () => AlertError.close();
